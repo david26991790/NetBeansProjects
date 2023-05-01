07700ae1588591a15df33f87cb323cf331f2534b
david26991790 <128453780+david26991790@users.noreply.github.com>
2023-05-01 10:54:27 +0800

    20230501

--- guesscode/src/guesscode/NumberGuess.java
+++ guesscode/src/guesscode/NumberGuess.java
@@ -1,39 +1,36 @@
 package guesscode;
 
-
-
-
+import java.util.Scanner;
 import java.util.Random;
 
 public class NumberGuess {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Random random = new Random();
         
-      
+       
         int answer = random.nextInt(100) + 1; 
-        System.out.println("1到100，猜猜看是多少？");
+        System.out.println("一個1到100之间的数字，你猜猜看是多少？");
         
         int guess; 
         int count = 0; 
         
         do {
-            guess = random.nextInt(100) + 1; 
-            
-            System.out.println("我猜是：" + guess);
+            System.out.print("請輸入你猜的數字：");
+            guess = input.nextInt(); 
             
             count++; 
             
             if (guess > answer) {
-                System.out.println("猜的数字太大了！");
+                System.out.println("你猜测的数字太大了，再試一次！");
             } else if (guess < answer) {
-                System.out.println("猜的数字太小了！");
+                System.out.println("你猜测的数字太小了，再試一次！");
             } else {
-                System.out.println("我猜對了！");
-                System.out.println("我猜了" + count + "次。");
+                System.out.println("恭喜你，猜對了！");
+                System.out.println("你猜了" + count + "次。");
             }
         } while (guess != answer);
+        
+        input.close();
     }
 }
-
-
-
